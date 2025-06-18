a955dc8baa9aee527a7345a5bcd794ec2a14e8a1
ptkaryapratamadunia <smallpart.ptkpd@gmail.com>
2025-06-18 13:53:26 +0700

    comm

--- app.py
+++ app.py
@@ -1581,7 +1581,8 @@ def cleaning_process(df):
 			
 			with zahra:	#Tabel NG% by Line& Kategori
 				st.write('NG (%) by Line & Kategori')
-				pt_kategori_line = pt_kategori_line.round(2)			
+				pt_kategori_line = pt_kategori_line.round(2)
+				pt_kategori_line = pt_kategori_line.map(format_with_comma)		
 				st.write(pt_kategori_line)
 			
 			#-----------------

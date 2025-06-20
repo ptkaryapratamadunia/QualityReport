c70ed1b08078f1c4972f3a2cff7e52d77823997c
ptkaryapratamadunia <smallpart.ptkpd@gmail.com>
2025-06-20 11:00:05 +0700

    movedown tabel

--- app.py
+++ app.py
@@ -1243,11 +1243,6 @@ def cleaning_process(df):
 			# Bulatkan nilai-nilai ke angka bulat terdekat
 			pt_customer_line2 = pt_customer_line2.map(format_with_comma)
 
-			st.write('Qty NG (lot) by Line & Customer')
-			pt_customer_line2_tranposed=pt_customer_line2.transpose()
-			with st.expander("Klik untuk melihat Tabel Qty NG (lot) by Line & Customer", expanded=False):
-				st.write(pt_customer_line2_tranposed)
-
 			sikir,sinan=st.columns(2)
 			
 			with sikir: #Grafik kolom Qty NG(lot) B4 by Cust.ID Ungu 09Jun25 show value inside bar
@@ -1398,6 +1393,11 @@ def cleaning_process(df):
 				)
 
 				st.plotly_chart(fig, use_container_width=True)
+
+			st.write('Tabel Qty NG (lot) by Line & Customer')
+			pt_customer_line2_tranposed=pt_customer_line2.transpose()
+			with st.expander("Klik untuk melihat Tabel Qty NG (lot) by Line & Customer", expanded=False):
+				st.write(pt_customer_line2_tranposed)
 
 			st.markdown("---")
 
